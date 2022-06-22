d0e5d92a4f6cd6c9a96f19e1e3a75e42ac752369
Mike <mwambugu868@gmail.com>
2022-06-23 02:16:07 +0300

    modified

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,12 @@
-function show() {
-    alert("Hello MIKE, Welcome to the bootcamp!");
+let username = document.getElementById("name");
+let butt = document.getElementById("butt");
+let par = document.getElementById("text");
+
+function Greeting(){
+    butt.addEventListener('click', ()=>{
+        par.innerText = `Hello ${username.value} , Welcome to bootcamp`;
+    })
 }
+
+
+Greeting();
